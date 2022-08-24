98de6fc20f7f4bf00627c46caf7c702b8e73c6ae
Prathibha <prsthibha@gmail.com>
2022-08-24 17:33:05 +0530

    other routes added

--- app.js
+++ app.js
@@ -19,14 +19,26 @@ app.get('/', (req, res) => {
   res.render('home', {homeStartingContent:homeStartingContent});
 });
 
+app.get('/about', function (req, res) {
+  res.render('about', {aboutContent:aboutContent});
+});
 
+app.get('/contact', function (req, res) {
+  res.render('contact', {contactContent:contactContent});
+});
 
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
 
+app.post('/compose', function (req, res) {
 
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
 
-
-
-
+});
 
 
 
